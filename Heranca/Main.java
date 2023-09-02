968f4c7862aa6b31756cd89b3ea476ec6a4ff756
Felipe Andreolla <132094407+FelipeAndreolla@users.noreply.github.com>
2023-09-02 18:31:10 -0300

    atualização de dados heranca

---
+++ Heranca/Main.java
@@ -0,0 +1,12 @@
+package Heranca
+
+public class Main {
+    public static void main(String[] args){
+        Pessoa p = new Pessoa();
+        p.nome = "Joao";
+
+        Vendedor v = new Vendedor();
+        v.nome = "roberto";
+        v.comissao = 15;
+    }
+}
