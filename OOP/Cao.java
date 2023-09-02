c9d68559c148a304cdb0dbd5dbc07d3d47e62826
Felipe Andreolla <132094407+FelipeAndreolla@users.noreply.github.com>
2023-09-02 10:49:42 -0300

    Aula criando classes e ojetos

---
+++ OOP/Cao.java
@@ -0,0 +1,12 @@
+package OOP;
+
+public class Cao {
+    String nome;
+    String cor;
+    int idade;
+    double peso;
+
+    public void Anda(){
+        System.out.println("Estou andando...");
+    }
+}
---
+++ OOP/Main.java
@@ -0,0 +1,12 @@
+package OOP;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        Cao cachorro = new Cao();
+        cachorro.nome = "Bob";
+        cachorro.Anda();
+
+        System.out.println(cachorro.nome);
+    }
+}
